c3a452f6ca562cfb6a79a6df2fc9266698b9df7c
Alex Paredes <alexparedesr95@gmail.com>
2022-02-08 23:20:02 -0600

    FEAT: Created file to have shared heroes DB.

---
+++ src/js/heroesDB.js
@@ -0,0 +1,14 @@
+export const heroesDB = {
+    cap: {
+        nombre: 'Capitán América',
+        poder: 'Aguantar inyecciones mutantes.'
+    },
+    iron: {
+        nombre: 'Ironman',
+        poder: 'Ser MUY rico.'
+    },
+    spider: {
+        nombre: 'Spiderman',
+        poder: 'Ser pro veneno de arañas.'
+    },
+};
