7155984b9a31d77ec04cca31e3d49d7b8c3d48da
Alex Paredes <alexparedesr95@gmail.com>
2022-02-08 23:20:27 -0600

    REFACTOR: Moved Heroes DB to another file and imported it.

--- src/js/calbacks.js
+++ src/js/calbacks.js
@@ -1,21 +1,8 @@
-const heroes = {
-    cap: {
-        nombre: 'Capitán América',
-        poder: 'Aguantar inyecciones mutantes.'
-    },
-    iron: {
-        nombre: 'Ironman',
-        poder: 'Ser MUY rico.'
-    },
-    spider: {
-        nombre: 'Spiderman',
-        poder: 'Ser pro veneno de arañas.'
-    },
-};
+import { heroesDB } from './heroesDB';
 
 export const buscarHeroe = ( id, callback ) => {
 
-    const heroe = heroes[ id ];
+    const heroe = heroesDB[ id ];
 
     if ( heroe ) {
         callback( null, heroe );
