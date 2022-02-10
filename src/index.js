20373d891fa34f984e5303f7439ca043dbf4db18
Alex Paredes <alexparedesr95@gmail.com>
2022-02-09 20:02:47 -0600

    FEAT: Implemented standard and async version of buscarHeroe method.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import { promesaLenta, promesaMedia, promesaRapida } from './js/promesas';
+import { promesaLenta, promesaMedia, promesaRapida, buscarHeroe, buscarHeroeAsync } from './js/promesas';
 
 
 /* promesaLenta.then( console.log );
 promesaMedia.then( console.log );
 promesaRapida.then( console.log ); */
 
-Promise.race([ promesaLenta, promesaMedia, promesaRapida ])
+/* Promise.race([ promesaLenta, promesaMedia, promesaRapida ])
     .then( console.log )
+    .catch( console.warn ); */
+
+buscarHeroe( 'cap' )
+    .then( heroe => console.log( heroe ) )
+    .catch( console.warn );
+
+buscarHeroeAsync( 'spider' )
+    .then( heroe => console.log( heroe ) )
     .catch( console.warn );
 
